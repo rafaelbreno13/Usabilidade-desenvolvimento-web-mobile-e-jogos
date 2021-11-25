a1d3336b4701717ae980b7cc4e4a135a70a760cd
Breno Rodrigues <89614832+rafaelbreno13@users.noreply.github.com>
2021-11-25 12:50:35 -0300

    insert

---
+++ pratica19.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:uc_usabilidade_desenvolvimento_web_mobile_e_jogos_flutter/pratica19/rotas/primeira_rota.dart';
+
+void main() => runApp(
+      MaterialApp(
+        home: PrimeiraRota(),
+      ),
+    );
